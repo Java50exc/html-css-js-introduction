6abb43d22b5fcae5f556a2384ebf859b037c9741
allanteone <allanteone@gmail.com>
2024-02-08 15:12:44 +0200

    hw-24 final fixes

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import {DataForm as Form} from "./ui/input-data-form.js";
 import {VideoPlayer as Player} from "./ui/video-player.js";
 import { checkPlayingTime } from "./service/checkTime.js";
-import { mySleep, sleep } from "./utils/sleep.js";
-
+import { mySleep } from "./utils/sleep.js";
 
 const form = new Form("form-section");
 const player = new Player("video-section");
 
-
-async function playVideo(obj) {
+function playVideo(obj) {
     let res = checkPlayingTime(obj.time);
 
     if (res) {
@@ -16,8 +14,7 @@ async function playVideo(obj) {
     }
     player.setUrl(obj.video);
     player.start();
-    await mySleep(player, obj.time);
-    player.stop();  
+    mySleep(player, obj.time).then(() => player.stop()); 
 }
 
 form.addHandler(playVideo);
--- src/utils/sleep.js
+++ src/utils/sleep.js
@@ -1,19 +1,18 @@
-import { VideoPlayer } from "../ui/video-player.js";
-
 export function sleep(timeout) {
     return new Promise(resolve => {
         setTimeout(() => resolve(), timeout)
-    })
+    });
 }
 
 export function mySleep(player, timeout) {
     return new Promise(resolve => {
         const startTime = player.getPlayingTime();
+        
         setInterval(() => {
-            if (player.getPlayingTime() >= timeout + startTime || 
-            player.getPlayingTime == player.getDuration()) {
-                    resolve();
+            if (player.getPlayingTime() >= timeout + startTime ||
+                player.getPlayingTime == player.getDuration()) {
+                resolve();
             }
-        }, 100);   
+        }, 100);
     });
 }
